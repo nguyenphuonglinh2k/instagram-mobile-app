bde029ec9def10db544dd3be5d201911514e3893
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2022-06-20 16:47:27 +0700

    update read more/less button

--- src/components/sn-root/Post/PostContent.js
+++ src/components/sn-root/Post/PostContent.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import PropTypes from "prop-types";
 
 const PostContent = ({ content, imageContentSrc, ...otherProps }) => {
+  const [numberOfLines, setNumberOfLines] = useState(2);
+
+  const onToggleContentLength = () => {
+    if (numberOfLines === 0) {
+      setNumberOfLines(2);
+    } else {
+      setNumberOfLines(0);
+    }
+  };
+
   return (
     <View {...otherProps}>
-      <Text numberOfLines={2} style={styles.content}>
+      <Text numberOfLines={numberOfLines} style={styles.content}>
         {content}
       </Text>
-      <TouchableOpacity>
-        <Text style={styles.viewMore}>View more</Text>
+      <TouchableOpacity onPress={onToggleContentLength}>
+        <Text style={styles.viewMore}>
+          {numberOfLines === 0 ? "View less" : "View more"}
+        </Text>
       </TouchableOpacity>
       <Image source={{ uri: imageContentSrc }} style={styles.image} />
     </View>
